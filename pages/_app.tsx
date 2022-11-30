5f4374df350ca9c6b9fd3aae6bfae25963bf722c
Dwight McLeish Jr <dmcleishjr@gmail.com>
2022-11-30 02:30:20 -0500

    chore: added title metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Fragment } from 'react';
 import Script from 'next/script';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Fragment>
+			<Head>
+				<title>Dwight Mcleish | Developer</title>
+			</Head>
 			<div className='canvas'>
 				<canvas className='connecting-dots'></canvas>
 			</div>
